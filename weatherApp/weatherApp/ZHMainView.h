eef5a419be785667c7a987b98a72eaef75e4734b
zhuchuangang <zhuchuangang20@qq.com>
2014-11-06 21:35:02 +0800

    新增天气主页面自定义view

---
+++ weatherApp/weatherApp/ZHMainView.h
@@ -0,0 +1,18 @@
+//
+//  ZHMainView.h
+//  weatherApp
+//
+//  Created by zcg on 14/11/6.
+//  Copyright (c) 2014年 zcg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZHMainView : UIView
+
+@property (strong, nonatomic) IBOutlet UILabel *cityLabel;
+@property (strong, nonatomic) IBOutlet UILabel *weatherLabel;
+@property (strong, nonatomic) IBOutlet UILabel *tempLabel;
+
+
+@end
---
+++ weatherApp/weatherApp/ZHMainView.m
@@ -0,0 +1,23 @@
+//
+//  ZHMainView.m
+//  weatherApp
+//
+//  Created by zcg on 14/11/6.
+//  Copyright (c) 2014年 zcg. All rights reserved.
+//
+
+#import "ZHMainView.h"
+
+@implementation ZHMainView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+
+
+@end
